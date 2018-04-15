707250552d08250d268a323d5b05805b05e13e23
Dylan Chan <dillchan10@gmail.com>
2018-04-15 22:31:16 +0100

    Restructured Folders

--- JavaCal/test/main/java/CalculatorTest.java
+++ JavaCal/test/main/java/CalculatorTest.java
@@ -7,39 +7,34 @@ import static org.junit.Assert.*;
 public class CalculatorTest {
 
     Calculator cal = new Calculator();
-
     @Test
     public void add()
     {
-
         int num1 = 2;
         int num2 = 3;
         assertEquals(5,cal.add(num1,num2));
     }
 
     @Test
-    public void sub()
-    {
-      int num1=20;
-      int num2=5;
-      assertEquals(15,cal.sub(num1,num2));
+    public void sub() {
+
+        int num1=20;
+        int num2=5;
+        assertEquals(15,cal.sub(num1,num2));
     }
 
     @Test
-    public void mult()
-    {
+    public void mult() {
+
         int num1 = 5;
         int num2 = 5;
         assertEquals(25,cal.mult(num1,num2));
-
     }
 
     @Test
-    public void div()
-    {
+    public void div() {
         int num1 = 150;
         int num2 = 10;
         assertEquals(15, cal.div(num1,num2));
-
     }
 }
