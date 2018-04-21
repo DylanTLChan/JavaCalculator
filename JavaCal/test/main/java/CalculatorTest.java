d81aeda6153709d964ec9dc6e2ca900d971e84bf
Dylan Chan <dillchan10@gmail.com>
2018-04-21 13:57:38 +0100

    Test to make build fail..

--- JavaCal/test/main/java/CalculatorTest.java
+++ JavaCal/test/main/java/CalculatorTest.java
@@ -12,7 +12,7 @@ public class CalculatorTest {
     {
         int num1 = 2;
         int num2 = 3;
-        assertEquals(6,cal.add(num1,num2));
+        assertEquals(67,cal.add(num1,num2));
     }
 
     @Test
