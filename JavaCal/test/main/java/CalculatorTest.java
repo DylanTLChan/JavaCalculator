f95357b8084ac65f3d5c5bde4eb06e43f709fafc
Dylan Chan <dillchan10@gmail.com>
2018-04-21 14:09:36 +0100

    Succesfully Tested Fail Build in Jenkins changed back the code to pass.

--- JavaCal/test/main/java/CalculatorTest.java
+++ JavaCal/test/main/java/CalculatorTest.java
@@ -12,7 +12,7 @@ public class CalculatorTest {
     {
         int num1 = 2;
         int num2 = 3;
-        assertEquals(89,cal.add(num1,num2));
+        assertEquals(5,cal.add(num1,num2));
     }
 
     @Test
