911440fe87d43310b2365f3d3134ff3a4e2b3eeb
Dylan Chan <dillchan10@gmail.com>
2018-04-27 12:00:50 +0100

    Changed Test

--- JavaCal/test/main/java/CalculatorTest.java
+++ JavaCal/test/main/java/CalculatorTest.java
@@ -13,7 +13,7 @@ public class CalculatorTest {
     {
         int num1 = 2;
         int num2 = 3;
-        assertEquals(5,cal.add(num1,num2));
+        assertEquals(7,cal.add(num1,num2));
     }
 
     @Test
